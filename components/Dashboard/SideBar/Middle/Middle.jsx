f86b9171b4e1c571db619763156f9626ed5e9c8f
anshkush92college <imt_2020013@iiitm.ac.in>
2022-12-10 16:25:57 +0530

    :bug: Changed the overall padding-x of the middle section using the calc() function for better accuracy

--- components/Dashboard/SideBar/Middle/Middle.jsx
+++ components/Dashboard/SideBar/Middle/Middle.jsx
@@ -5,7 +5,7 @@ import Common from "./Cards/Common/Common";
 
 const Middle = () => {
   return (
-    <section className="ml-60 flex flex-col flex-grow py-6 justify-center gap-y-10 items-start  pl-5 pr-9">
+    <section className="ml-60 flex flex-col flex-grow py-6 justify-center gap-y-10 items-start  pl-[calc(240px_-_232px)] pr-[calc(240px_-_216px)]">
       <Search></Search>
       <Common></Common>
     </section>
