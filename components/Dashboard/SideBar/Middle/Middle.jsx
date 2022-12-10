0c9d21ce7487ba32c77a56a88880eedd4b12316e
anshkush92college <imt_2020013@iiitm.ac.in>
2022-12-10 13:36:57 +0530

    :sparkles: Added the font styles in the tailwid config and global.css so it reflect on the whole app
    See the docs for reference on how to configure your tailwind.config.js file for better styles

--- components/Dashboard/SideBar/Middle/Middle.jsx
+++ components/Dashboard/SideBar/Middle/Middle.jsx
@@ -4,7 +4,7 @@ import Search from "./Search/Search";
 
 const Middle = () => {
   return (
-    <section className="bg-black flex flex-col flex-grow py-6 justify-center">
+    <section className="flex flex-col flex-grow py-6 justify-center">
       <Search></Search>
     </section>
   );
