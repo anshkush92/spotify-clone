dfd2ddcd798c9a54bf9335085f8939a99a5ada10
anshkush92college <imt_2020013@iiitm.ac.in>
2022-12-11 17:49:29 +0530

    :sparkles: Added the instance of the spotifyWebApi in the Dashboard, and using access token in the Middle.jsx to authenticate make sure that the spotifyWebApi wrapper works, using useEffect() to setAccessToken to the spotifyWebApi

--- components/Dashboard/Dashboard.jsx
+++ components/Dashboard/Dashboard.jsx
@@ -4,6 +4,15 @@ import Left from "./SideBar/Left/Left";
 import Middle from "./SideBar/Middle/Middle";
 import Right from "./SideBar/Right/Right";
 
+import SpotifyWebApi from "spotify-web-api-node";
+
+// Creating an instance of SpotifyWebApi ---> Wrapper for SPOTIFY API
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+  redirectUri: "http://localhost:3000/api/auth/callback/spotify",
+});
+
 const Dashboard = () => {
   return (
     <main>
--- components/Dashboard/SideBar/Middle/Middle.jsx
+++ components/Dashboard/SideBar/Middle/Middle.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { useSession } from "next-auth/react";
+
 import Search from "./Search/Search";
 import Common from "./Cards/Common/Common";
 import Artist from "./Cards/Artist/Artist";
@@ -7,6 +9,17 @@ import Browse from "./Cards/Browse/Browse";
 import Home from "./Cards/Home/Home";
 
 const Middle = () => {
+  const { data: session } = useSession();
+  // Similar to the API key, need for the Spotify to make request to its API
+  const { accessToken } = session;
+
+  // useEffect() runs every time the accessToken changes as it is mentioned in the dependency array
+  useEffect(() => {
+    if (!accessToken) return;
+    // Setting the access token to the spotifyApi instance, otherwise it will not work
+    spotifyApi.setAccessToken(accessToken);
+  }, [accessToken]);
+
   return (
     <section className="ml-60 flex flex-col flex-grow py-6 justify-center gap-y-8 items-start  pl-[calc(240px_-_232px)] pr-[calc(240px_-_216px)]">
       <Search></Search>
